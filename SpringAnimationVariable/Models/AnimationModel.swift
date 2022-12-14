283391c31d33c5804d4acb9b0239a0f7661aac1e
VK <art.nebar@mail.ru>
2022-12-14 16:36:19 +0300

    Relize

---
+++ SpringAnimationVariable/Models/AnimationModel.swift
@@ -0,0 +1,16 @@
+//
+//  AnimationModel.swift
+//  SpringAnimationVariable
+//
+//  Created by Vladimir Khalin on 14.12.2022.
+//
+
+import Foundation
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+}
---
+++ SpringAnimationVariable/ViewControllers/SpringAnimationViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  SpringAnimationVariable
+//
+//  Created by Vladimir Khalin on 14.12.2022.
+//
+import Foundation
+import UIKit
+import SpringAnimation
+
+class ViewController: UIViewController {
+    @IBOutlet var animationSpringView: SpringImageView!
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var infoAnimationLabels: [SpringLabel]!
+    @IBOutlet var stackInfoLabels: UIStackView!
+    
+    private var animation: Animation!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        animationSpringView.layer.cornerRadius = 20
+        startButton.layer.cornerRadius = 20
+        animationSpringView.addSubview(stackInfoLabels)
+        
+        makeAnimation()
+        enterInfoToLabels()
+    }
+    
+    @IBAction func startAnimationButtonPresed(_ sender: SpringButton) {
+        
+        animationSpringView.animation = animation.preset
+        animationSpringView.curve = animation.curve
+        animationSpringView.force = animation.force
+        animationSpringView.duration = animation.duration
+        animationSpringView.delay = animation.delay
+        animationSpringView.animate()
+        
+        enterInfoToLabels()
+        makeAnimation()
+        
+        startButton.setTitle("Next: \(animation.preset)", for: .normal)
+    }
+    
+    private func makeAnimation() {
+        
+        guard let preset = AnimationPreset.allCases.randomElement() else { return }
+        guard let curve = AnimationCurve.allCases.randomElement() else {return }
+        let force = isRandomDouble()
+        let duration = isRandomDouble()
+        let delay = isRandomDouble()
+        
+        animation = Animation(preset: preset.rawValue, curve: curve.rawValue, force: force, duration: duration, delay: delay)
+    }
+    
+    private func isRandomDouble() -> Double {
+        let doubleNumber = (Double.random(in: 0...3)*100).rounded()/100
+        return doubleNumber
+    }
+    
+    private func enterInfoToLabels() {
+        for lable in 0..<infoAnimationLabels.count {
+            switch lable {
+            case 0: infoAnimationLabels[lable].text = "Preset: \(animation.preset)"
+            case 1: infoAnimationLabels[lable].text = "Curve: \(animation.curve)"
+            case 2: infoAnimationLabels[lable].text = "Force: \(animation.force)"
+            case 3: infoAnimationLabels[lable].text = "Duration: \(animation.duration)"
+            default: infoAnimationLabels[lable].text = "Delay: \(animation.delay)"
+            }
+        }
+    }
+}
+
